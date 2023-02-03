0c5d7fc96ffaf23e74353c9e268b33d30efb3398
Michael Trang <mtrang@woolworths.com.au>
2023-02-03 14:36:44 +1100

    Use dynamic import

--- pages/posts/dynamic-header.js
+++ pages/posts/dynamic-header.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { DynamicHeader } from '../../components/DynamicHeader';
+import { AEMText } from '../../components/AEMText';
 // import { StaticHeader } from '../../components/StaticHeader';
 import Layout from '../../components/layout';
 
@@ -13,6 +14,7 @@ export default function FirstPost() {
       </Head>
       <h1>Dynamic Header</h1>
       <DynamicHeader />
+      <AEMText />
       {/* <StaticHeader /> */}
       <p>Welcome to Dynamic Header post!</p>
       <h2>
