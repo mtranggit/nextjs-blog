3a858cbd66c922b71f0160f1fece9c340315623d
Michael Trang <mtrang@woolworths.com.au>
2023-02-03 16:05:12 +1100

    no component imports

--- components/import-components.js
+++ components/import-components.js
@@ -17,12 +17,12 @@ import { MapTo } from '@adobe/aem-react-editable-components'
 // import AEMImage from './image'
 
 // import components with dynamic
-import { AEMText } from './AEMText'
-import { AEMTitle } from './AEMTitle'
-import { AEMImage } from './AEMImage'
+// import { AEMText } from './AEMText'
+// import { AEMTitle } from './AEMTitle'
+// import { AEMImage } from './AEMImage'
 
 const { NEXT_PUBLIC_AEM_SITE } = process.env;
 
-MapTo(`${NEXT_PUBLIC_AEM_SITE}/components/title`)(AEMTitle)
-MapTo(`${NEXT_PUBLIC_AEM_SITE}/components/text`)(AEMText)
-MapTo(`${NEXT_PUBLIC_AEM_SITE}/components/image`)(AEMImage)
+// MapTo(`${NEXT_PUBLIC_AEM_SITE}/components/title`)(AEMTitle)
+// MapTo(`${NEXT_PUBLIC_AEM_SITE}/components/text`)(AEMText)
+// MapTo(`${NEXT_PUBLIC_AEM_SITE}/components/image`)(AEMImage)
